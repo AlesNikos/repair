6455ff9cd28c11336b8e459929b8a64a1d63c5a4
Alexey Kostyuchik <alesnikos@gmail.com>
2019-06-03 23:00:42 +0500

    Добавил секцию портфолио и owl Carousel

--- src/js/modal.js
+++ src/js/modal.js
@@ -26,28 +26,56 @@ $(document).ready(function() {
   });
 
   /* Инициализация Slick слайдера */
-  $('.slider').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    prevArrow: $('.arrows__left'),
-    nextArrow: $('.arrows__right'),
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
+  // $('.slider').slick({
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  //   prevArrow: $('.arrows__left'),
+  //   nextArrow: $('.arrows__right'),
+  //   responsive: [
+  //     {
+  //       breakpoint: 1200,
+  //       settings: {
+  //         slidesToShow: 2,
+  //         slidesToScroll: 1
+  //       }
+  //     },
+  //     {
+  //       breakpoint: 992,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1
+  //       }
+  //     }
+  //   ]
+  // });
+
+
+  /* Инициализация Owl Carousel */
+  $('.owl-carousel').owlCarousel({
+      loop: true,
+      dots: false,
+      margin: 30,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        992: {
+          items: 2,
+        },
+        1200: {
+          items: 3,
         }
       }
-    ]
   });
-
+  
+  var owl = $('.owl-carousel');
+  owl.owlCarousel();
+  $('.arrows__left').click(function() {
+    owl.trigger('prev.owl.carousel');
+  });
+  $('.arrows__right').click(function () {
+    owl.trigger('next.owl.carousel');
+  });
 
 });
