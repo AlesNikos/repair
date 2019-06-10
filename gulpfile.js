6a5988a67e2bbcc328e64fe8e7e2e57f3d854c03
Alexey Kostyuchik <alesnikos@gmail.com>
2019-06-11 00:30:13 +0500

    Оптимизация проекта

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+let gulp = require('gulp');
+let cleanCSS = require('gulp-clean-css');
+let rename = require('gulp-rename');
+let uglify = require('gulp-uglify');
+let htmlmin = require('gulp-htmlmin');
+let tinyPNG = require('gulp-tinypng-compress');
+
+gulp.task('default', defaultTask);
+
+function defaultTask(done) {
+  done();
+}
+
+gulp.task('minify-css', function(done) {
+  return gulp.src('./src/css/*.css')
+  .pipe(cleanCSS({
+    compatibility: 'ie8'
+  }))
+  .pipe(gulp.dest('dist/css/'))
+  done();
+});
+
+gulp.task('minify-js', function (done) {
+  return gulp.src(['./src/js/*.js', '!./src/js/*.min.js']) 
+  .pipe(uglify()) 
+  .pipe(rename({
+    suffix: '.min'
+  })) 
+  .pipe(gulp.dest('dist/js/')) 
+  done();
+});
+
+gulp.task('move-js', function (done) {
+  return gulp.src('./src/js/*.min.js')
+  .pipe(gulp.dest('dist/js/'))
+  done();
+});
+
+gulp.task('htmlmin', function (done) {
+  return gulp.src('src/*.html')
+  .pipe(htmlmin({
+    collapseWhitespace: true
+  }))
+  .pipe(gulp.dest('dist/'))
+  done();
+});
+
+gulp.task('fonts', function (done) {
+  return gulp.src('./src/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts/'))
+  done();
+});
+
+gulp.task('tinypng', function () {
+  return gulp.src('./src/img/**/*.{png,jpg,jpeg}')
+    .pipe(tinyPNG({
+      key: 's5pturJNj4eRPPUIfkIyTXQQAovgcuj6',
+      sigFile: 'images/.tinypng-sigs',
+      log: true
+    }))
+    .pipe(gulp.dest('dist/img/'))
+    done();
+});
